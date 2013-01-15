8d875b750e30d64114a3f720e1832ce042ae074a
Trippley <trippley@doujinscene.com>
2013-01-15 22:17:43 +0100

    Added Text Support Including loading
    May not work or so I heard, as textures don't work yet. Loading should
    work.

---
+++ src/text.h
@@ -0,0 +1,27 @@
+
+#ifndef TEXT_H
+#define TEXT_H
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_tff.h>
+
+typedef enum {
+		LAY_LEFT,
+		LAY_CENTER,
+		LAY_RIGHT
+} Align;
+
+
+void draw_string(Align layout, float x, float y, const char *string, TTF_Font *font);
+void init_text(void);
+int stringcalculatewidth(char *string, TTF_Font *font);
+int stringcalculateheight(char *string, TTF_Font *font);
+int charcalculatewidth(char c, TTF_Font *font);
+
+struct Font { 
+		TTF_Font *main;
+};
+
+extern struct Font _font;
+
+#endif
